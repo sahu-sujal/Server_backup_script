364e94e52739e9a22c84cb99855df2d2f2a70efe
sahu-sujal <sujal020404@gmail.com>
2025-01-22 00:30:39 +0530

    backup script completed

---
+++ backup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+<< "readme"
+Author: Sujal Sahu
+Version: v1.0
+Usage: ./bachup.sh <source Path> <backup Path>
+
+readme
+
+#variables assigning
+source_dir=$1
+backup_dir=$2
+timestamp=$(date '+%Y-%m-%d-%H-%M-%S')
+S3_BUCKET="s3://serverbackuptesting/backup"
+
+function display_usage {
+    if [[ $? -ne 2 ]]; then
+        echo "kindly add the arugument"
+        echo "usage: $0 <path to source> <backup folder path>"
+        exit 1
+    fi
+
+}
+
+function create_backup {
+
+     if zip -r "${bachup_dir}/backup_${timestamp}.zip" "${source_dir}" 2>/dev/null; then
+        echo "bachup Generated Successfully backup_${timestamp}"
+    fi
+}
+
+function cleaning {
+
+        backups=($(ls -t "${backup_dir}/backup_"*.zip 2>/dev/null))
+
+        if [ "${#backups[@]}" -gt 5 ]; then
+                echo "perform cleaning"
+                removing_files=("${backups[@]:5}")
+
+                for backup in "${removing_files[@]}";
+                do
+                        rm -f ${backup}
+                done
+        fi
+
+        s3_bucket_sync
+}
+
+function s3_bucket_sync {
+        if ! aws s3 sync "$BACKUP_FOLDER" "$S3_BUCKET" --delete; then
+                echo "unable to sync with s3"
+                exit 1
+        fi
+}
+
+if [ $# -eq 0 ]; then
+        display_usage "$#"
+        exit 1
+fi
+
+if ! create_backup; then
+        echo "unable to create backup"
+        exit 1
+fi
+
+if ! cleaning; then
+        echo "unable to cleane backups"
+        exit 1
+
+fi
